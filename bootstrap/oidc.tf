24c1ea6c53705ce71a6972a7a37681231bc23f5d
tanvir039 <tanvir039@gmail.com>
2026-09-21 20:43:36 +0100

    debug: modified OIDC subject for both workflows

--- bootstrap/oidc.tf
+++ bootstrap/oidc.tf
@@ -21,7 +21,7 @@ resource "aws_iam_role" "github_ecr_push" {
       Action    = "sts:AssumeRoleWithWebIdentity"
       Condition = {
         StringEquals = { "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com" }
-        StringLike   = { "token.actions.githubusercontent.com:sub" = "repo:tanvir039/ecs-vscode-webapp:*" }
+        StringLike   = { "token.actions.githubusercontent.com:sub" = "repo:tanvir039@151540651/ecs-vscode-webapp@1366439722:ref:refs/heads/main" }
       }
     }]
   })
@@ -69,10 +69,13 @@ resource "aws_iam_role" "github_terraform_deploy" {
       Principal = { Federated = aws_iam_openid_connect_provider.github.arn }
       Action    = "sts:AssumeRoleWithWebIdentity"
       Condition = {
-        StringEquals = { "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com" }
-        StringLike   = { "token.actions.githubusercontent.com:sub" = "repo:tanvir039@151540651/ecs-vscode-webapp@1366439722:ref:refs/heads/main" }
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+            "token.actions.githubusercontent.com:sub" = "repo:tanvir039@151540651/ecs-vscode-webapp@1366439722:ref:refs/heads/main"
+          }
+        }
       }
-    }]
+    ]
   })
 }
 
