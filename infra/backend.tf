e64c775256000ddc22b772365c4961c76463e9c0
tanvir039 <tanvir039@gmail.com>
2026-09-21 17:54:11 +0100

    add: /backend.tf (s3 bucket)

---
+++ infra/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket       = "ecs-code-server-tanvir-tfstate"
+    key = "dev/terraform.tfstate"    
+    region       = "eu-west-2"
+    encrypt      = true
+    use_lockfile = true
+  }
+}
